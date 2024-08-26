624b24ef45c9a2ebc40e324893731bc42041ca61
Hudyno <hudyyno@gmail.com>
2024-08-26 16:09:28 +0200

    Add missing @JsonIgnoreProperties to model

--- src/main/java/com/crypto/cryptocompare/api/data/response/AssetSummary.java
+++ src/main/java/com/crypto/cryptocompare/api/data/response/AssetSummary.java
@@ -1,9 +1,11 @@
 package com.crypto.cryptocompare.api.data.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AssetSummary {
 
     @JsonProperty("TYPE")
